55e359104d9107eff47f2bc4fbc6b2dd69eeed60
Gurten7 <geertenliesker@hotmail.com>
2025-04-04 08:58:37 +0200

    Update service-worker.js

--- slimste/service-worker.js
+++ slimste/service-worker.js
@@ -1,28 +1,22 @@
-const CACHE_NAME = "slimste-kameraad-v1";
-const urlsToCache = [
-  "/slimste/index.html",
-  "/slimste/manifest.json",
-  "/slimste/icon-192.png",
-  "/slimste/icon-512.png",
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js",
-  "https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js",
-  "https://raw.githubusercontent.com/Gurten7/Timer-ppt/main/timer-sound.mp3",
-  "https://raw.githubusercontent.com/Gurten7/Timer-ppt/main/audio/timer-sound-end.mp3",
-  "https://raw.githubusercontent.com/Gurten7/Timer-ppt/main/audio/Sound-finale.mp3"
+const cacheName = 'slimste-kameraad-v1';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://raw.githubusercontent.com/Gurten7/Timer-ppt/main/audio/timer-sound-end.mp3',
+  'https://raw.githubusercontent.com/Gurten7/Timer-ppt/main/audio/Sound-finale.mp3'
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request).then(res => res || fetch(evt.request))
   );
 });
